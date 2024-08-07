50d6ab6f0f2d0865d7de07826b164d6d2c432ce3
AlpacaCoder1 <rushi.jariwala13@gmail.com>
2024-08-07 19:58:36 -0400

    added a comment

--- Day3.java
+++ Day3.java
@@ -1,14 +1,15 @@
 public class Day3 {
     public static void main(String[] args) {
-        double x = 133;
+        double x = 133; // sets x to 133
 
-        if (x < 100) {
+        if (x < 100) { // if x is less than 100 then add 50 to x
             x +=50;
         }  
-        if (x > 100) {
+        if (x > 100) { // if x is more than 100 than divide x by 20
              x /=20;
         }
-        System.out.println("\n" + "x = " + x + "\n");
+        System.out.println("\n" + "x = " + x + "\n"); // print the value of x
     }
-    
+    // I would rather be 11 feet tall
+    /* I would want to be 11 feet tall because it makes me the tallest person in the world. */
 }
