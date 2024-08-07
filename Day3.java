0830fd5267aef9a779e51187831920fe7ede2807
AlpacaCoder1 <rushi.jariwala13@gmail.com>
2024-08-07 19:44:59 -0400

    Integrated conditional statements

--- Day3.java
+++ Day3.java
@@ -1,11 +1,13 @@
 public class Day3 {
     public static void main(String[] args) {
-        int x = 2;
-        int y = 4;
-        x++;
-        y--;
-        int z = x + y;
-        System.out.println("\n" + z + "\n");
+        int x = 120;
+        if (x < 100) {
+            x+=50;
+        }  
+        if (x > 100) {
+             x/=20;
+        }
+        System.out.println("\n" + x + "\n");
     }
     
 }
