e79254911d6cf0337f3570a1f09d3d27a12fc9b2
AlpacaCoder1 <rushi.jariwala13@gmail.com>
2024-08-07 18:59:18 -0400

    Added mutable and immutable variables and printed them out

--- pig.java
+++ pig.java
@@ -1,5 +1,10 @@
 public class pig {
     public static void main(String[] args) {
+        int number = 2;
+        System.out.println("\n" + number);
+        final String FINAL_STRING = "Prog da best";
+        System.out.println("\n" + FINAL_STRING);
         System.out.println("\n" + "Oink Oink" + "\n");
+   
     }
 }
