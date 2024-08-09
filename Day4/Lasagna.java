00c2001a0e69edfa5586ee778c074f9d119cbcb3
AlpacaCoder1 <rushi.jariwala13@gmail.com>
2024-08-09 19:26:59 -0400

    completed task 2

--- Day4/Lasagna.java
+++ Day4/Lasagna.java
@@ -1,7 +1,13 @@
 public class Lasagna {
     int minutesExpected = 40;
-    
+
     public int expectedMinutesInOven() {
         return minutesExpected;
+    }
+    public int remainingMinutesInOver(int minutesTakenInOven) {
+        return minutesExpected - minutesTakenInOven;
+    }
+    public int preparationTimeInMinutes (int layers) {
+        return 2 * layers;
     }
 }
