02ccd9a58fad24cb671e1ba8276357ed609eee6c
AlpacaCoder1 <rushi.jariwala13@gmail.com>
2024-08-09 09:55:25 -0400

    called methods

--- Action.java
+++ Action.java
@@ -3,6 +3,11 @@ public class Action {
       Person rushi = new Person();
       rushi.printAge();
       rushi.ageOlder();
+      rushi.setAge(16);
+      rushi.setAgeAndName(17,"Richard");
+      rushi.getter();
+      rushi.printAge();
+      rushi.printName();
    }
    
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,7 @@
 public class Person {
       int age = 14;
+      String name = "Rushi";
+
    public void printAge() {
       System.out.println("Age: "+ age);
    }
@@ -10,6 +12,21 @@ public class Person {
    public void ageYounger() {
       age--;
       System.out.println("New Age: " + age);
+   }
+
+   public void setAge(int newAge) {
+      age = newAge;
+   }
+
+   public void setAgeAndName(int newAge, String newName) {
+      age = newAge;
+      name = newName;
+   }
+   public int getter() {
+      return age;
+   }
+   public void printName() {
+      System.out.println("Name: " + name);
    }
     
 }
