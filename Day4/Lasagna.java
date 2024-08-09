2145ddfd03c266a506ea2f50f8aecdf90e694010
AlpacaCoder1 <rushi.jariwala13@gmail.com>
2024-08-09 19:27:38 -0400

    added comments

--- Day4/Lasagna.java
+++ Day4/Lasagna.java
@@ -1,9 +1,10 @@
 public class Lasagna {
     int minutesExpected = 40;
-
+// Task #1
     public int expectedMinutesInOven() {
         return minutesExpected;
     }
+// Task #2
     public int remainingMinutesInOver(int minutesTakenInOven) {
         return minutesExpected - minutesTakenInOven;
     }
